69e277b7e4fd4b6c6d649a2ca0c9be56c919648c
giyoon-Park <giyoon.p@kakao.com>
2020-06-22 11:47:09 +0900

    2020.06.22 commit

--- testNodejs/conf/dbConfig.js
+++ testNodejs/conf/dbConfig.js
@@ -2,7 +2,23 @@
 const config = {
 		user: process.env.NODE_ORACLEDB_USER || "grapefruit",
 		password: process.env.NODE_ORACLEDB_PASSWORD || "increpas",
-		connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orcl"
+		connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orcl",
+		doRelease: function(connection, userlist){
+			connection.close(function(err){
+				if(err){
+					consle.error(err.message);
+				}
+				
+				// DB종료까지 모두 완료되었을 시 최종적으로 응답 데이터 반환
+				console.log('list size: ' + userlist.length);
+				
+				for (var i = 0; i < userlist.length; i++) {
+					console.log('name: ' + userlist[i][1]);
+				}
+				
+				response.send(userlist);
+			});
+		}
 }
 
 module.exports = config;
--- testNodejs/web.js
+++ testNodejs/web.js
@@ -5,12 +5,18 @@ var http = require('http');
 var bodyParser = require('body-parser');
 var app = express();
 
-// 오라클 자동 커밋
-oracledb.autoCommit = true;
-
 //라우팅 모듈 선언
 var indexRouter = require('./routes/index');
 var clubRouter = require('./routes/club/club');
+
+//mybatis-mapper 추가
+var mybatisMapper = require('mybatis-mapper');
+
+//Mapper Load
+mybatisMapper.createMapper([ './APP/mapper/oracle-mapper.xml' ]);
+
+// 오라클 자동 커밋
+oracledb.autoCommit = true;
 
 //express 서버 포트 설정(cafe24 호스팅 서버는 8001 포트 사용)
 app.set('port', process.env.PORT || 8001);
@@ -26,6 +32,11 @@ app.set('view engine', 'ejs');
 //req.body 를 사용하기 위한 설정
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.json()); // for parsing application/json
+
+//등록되지 않은 패스에 대해 페이지 오류 응답
+app.all('*', function(req, res) {
+res.status(404).send('<h1>ERROR - 페이지를 찾을 수 없습니다.</h1>');
+});
 
 //서버 생성
 http.createServer(app).listen(app.get('port'), function(){
